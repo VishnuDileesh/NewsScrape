d73b58316cf1ae1e59623fecc2ea396847db7c99
Pheyonix <dev@vishnudileesh.com>
2020-01-29 13:16:57 +0530

    Get latest 7 articles in the site addd urls to list

--- app.py
+++ app.py
@@ -4,6 +4,24 @@ import requests
 
 #grab data from thehackernews home page
 site = requests.get('https://thehackernews.com')
-print(site.status_code)
+
+site_data = site.text
+
+soup = BeautifulSoup(site_data, 'html.parser')
+
+print(soup.title)
+
+# loop through all blog posts in home page
+blog_posts = soup.find("div", class_="blog-posts")
+
+news_urls = []
+
+story_links = soup.find_all('a', class_="story-link")
 
 
+for story_link in  story_links:
+    url = story_link.get('href')
+    news_urls.append(url)
+
+
+print(news_urls)
