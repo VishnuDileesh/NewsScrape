31af9a5357e0553e304c0ced746f4579b219765f
Pheyonix <dev@vishnudileesh.com>
2020-01-29 13:53:26 +0530

    Add articles to class object

--- app.py
+++ app.py
@@ -3,6 +3,23 @@ from bs4 import BeautifulSoup
 import requests
 
 
+class Newsarticle:
+
+    def __init__(self, title, author, keywords, summary):
+        self.article_title = title
+        self.article_author = author
+        self.article_keywords = keywords
+        self.article_summary = summary
+
+    def get_article(self):
+        #print(self.article_title)
+        #print(self.article_author)
+        #print(self.article_keywords)
+        #print(self.article_summary)
+
+        return [self.article_title, self.article_author, self.article_keywords, self.article_summary]
+
+
 #grab data from thehackernews home page
 site = requests.get('https://thehackernews.com')
 
@@ -24,6 +41,8 @@ for story_link in  story_links:
     url = story_link.get('href')
     news_urls.append(url)
 
+list_articles = []
+
 
 for news_url in news_urls:
 
@@ -33,15 +52,34 @@ for news_url in news_urls:
 
     article.parse()
 
-    print(article.authors)
+#    print(article.title)
+#    print(article.publish_date)
+
+#    print(article.authors)
 
     article.nlp()
 
-    print(article.keywords)
+#    print(article.keywords)
 
-    print(article.summary)
+#    print(article.summary)
+
+    title = article.title
+
+    author = article.authors
+
+    keywords = article.keywords
+
+    summary = article.summary
+
+    article1 = Newsarticle(title, author, keywords, summary)
+
+
+    list_articles.append(article1)
 
 
 
 
+for article in list_articles:
+
+    article.get_article()
 
