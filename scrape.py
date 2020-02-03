812bd8a21ecd62245ee766bbb7524c310bae0d67
Pheyonix <dev@vishnudileesh.com>
2020-02-03 16:13:54 +0530

    Restructuring code: added scrape file which scrapes and saves data to db

---
+++ scrape.py
@@ -0,0 +1,82 @@
+from newspaper import Article
+from bs4 import BeautifulSoup
+import requests
+import sqlite3
+import os
+
+
+connection = sqlite3.connect('news_scrape.db')
+cursor = connection.cursor()
+
+class NewsArticle:
+
+    def __init__(self, news_urls):
+
+        self.news_urls = news_urls
+
+        for news_url in self.news_urls:
+
+            article = Article(news_url)
+
+            article.download()
+
+            article.parse()
+
+            self.title = article.title
+
+            self.author = article.authors[0]
+
+            article.nlp()
+
+            self.summary = article.summary
+
+            cursor.execute("""INSERT INTO Articlelist VALUES (?, ?, ?)""", (self.title, self.author, self.summary))
+
+
+
+            connection.commit()
+
+
+
+
+def scrape_news():
+
+    cursor.execute("DROP TABLE Articlelist")
+
+    print("Dropped Table")
+
+
+    print("Creating Table")
+    
+    cursor.execute(
+    """CREATE TABLE Articlelist(
+        title  TEXT,
+        author  TEXT,
+        summary  TEXT
+    )
+    """)
+
+    connection.commit()
+
+
+    print("Created Table")
+
+    site1_content = requests.get('https://thehackernews.com')
+
+    site1_data = site1_content.text
+
+    soup1 = BeautifulSoup(site1_data, 'html.parser')
+
+    news_urls = []
+
+    story_links1 = soup1.find_all('a', class_="story-link")
+
+    for story_link1 in story_links1:
+        url = story_link1.get('href')
+        news_urls.append(url)
+
+    site1 = NewsArticle(news_urls)
+        
+
+
+scrape_news()
